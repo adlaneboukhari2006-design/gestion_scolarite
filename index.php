9740905d54c5c8c9b3b1cac659cc24200bd5075b
adlaneboukhari2006-design <adlaneboukhari2006@gmail.com>
2026-04-16 20:48:58 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.php'; ?>
+<div class="hero">
+  <h1>🎓 Application de Gestion de Scolarité</h1>
+  <p>Gérez les étudiants, les notes et les relevés en toute simplicité.<br>
+     Plateforme dédiée à l'USTHB – Faculté d'Informatique.</p>
+  <a href="login.php" class="btn">Se connecter</a>
+</div>
+<?php include 'footer.php'; ?>
